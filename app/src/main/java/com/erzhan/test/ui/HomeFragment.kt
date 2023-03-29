ce285d6ea018feb8d2099856f1e6c022aaa4e379
Erzhan Anarkulov <anarkulov_e@auca.kg>
2023-03-29 11:53:36 +0600

    Complete ui home screen

--- app/src/main/java/com/erzhan/test/ui/HomeFragment.kt
+++ app/src/main/java/com/erzhan/test/ui/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
+import com.erzhan.test.R
 import com.erzhan.test.core.ui.BaseFragment
 import com.erzhan.test.ui.home.HomeViewModel
 import com.erzhan.test.databinding.FragmentHomeBinding
@@ -12,6 +15,42 @@ import com.erzhan.test.databinding.FragmentHomeBinding
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
     override val viewModel: HomeViewModel by viewModels()
+
+    override fun initView() {
+        super.initView()
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+        val spinnerAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            arrayOf("Telcom", "Safaricom")
+        )
+        binding.airtimeSpinner.adapter = spinnerAdapter
+
+        binding.airtimeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                when (position) {
+                    0 -> {
+                        binding.airtimeIcon.setImageResource(R.drawable.telcom)
+                    }
+                    1 -> {
+                        binding.airtimeIcon.setImageResource(R.drawable.safaricom)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
 
     override fun inflateViewBinding(
         inflater: LayoutInflater,
