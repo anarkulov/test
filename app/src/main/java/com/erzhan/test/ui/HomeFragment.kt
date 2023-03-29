74cc587a3e62392b4f57e51989ca19cfd6a2069d
nooken <nooken@deveem.io>
2023-03-29 14:09:23 +0600

    Add close animation

--- app/src/main/java/com/erzhan/test/ui/HomeFragment.kt
+++ app/src/main/java/com/erzhan/test/ui/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.erzhan.test.ui
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import android.animation.ValueAnimator.AnimatorUpdateListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
@@ -50,6 +55,12 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
                 // Do nothing
             }
         }
+
+        binding.bannerButtonClose.setOnClickListener {
+            binding.infoBanner.visibility = View.GONE
+        }
+
+
     }
 
     override fun inflateViewBinding(
