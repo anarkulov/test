a3fcc87d097776f47c75ce3a529b58c6f2f6fcdf
Erzhan Anarkulov <anarkulov_e@auca.kg>
2023-03-30 13:19:02 +0600

    Add chats / bottom sheet

--- app/src/main/java/com/erzhan/test/ui/home/HomeFragment.kt
+++ app/src/main/java/com/erzhan/test/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.erzhan.test.ui
+package com.erzhan.test.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,12 +7,15 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.viewModels
 import com.erzhan.test.R
 import com.erzhan.test.core.Utils
 import com.erzhan.test.core.ui.BaseFragment
 import com.erzhan.test.databinding.FragmentHomeBinding
-import com.erzhan.test.ui.home.HomeViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
@@ -21,6 +24,86 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
     override fun initView() {
         super.initView()
         initSpinner()
+        initBottomSheet()
+    }
+
+    private fun initBottomSheet() {
+        val bottomSheet = BottomSheetDialog(requireContext(), R.style.AppBottomSheetDialogTheme)
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+        bottomSheet.setContentView(bottomSheetView)
+
+        val airtelCheckBox = bottomSheetView.findViewById<CheckBox>(R.id.option_one_checkbox)
+        val safaricomCheckBox = bottomSheetView.findViewById<CheckBox>(R.id.option_two_checkbox)
+        val telcomCheckBox = bottomSheetView.findViewById<CheckBox>(R.id.option_three_checkbox)
+
+        val airtelLayout = bottomSheetView.findViewById<LinearLayout>(R.id.option_one)
+        val safaricomLayout = bottomSheetView.findViewById<LinearLayout>(R.id.option_two)
+        val telcomLayout = bottomSheetView.findViewById<LinearLayout>(R.id.option_three)
+
+        val btnSelect = bottomSheetView.findViewById<AppCompatButton>(R.id.btn_select)
+
+        fun update() {
+            when(binding.airtimeOperator.text.toString()) {
+                getString(R.string.airtel) -> {
+                    airtelCheckBox?.isChecked = true
+                    safaricomCheckBox?.isChecked = false
+                    telcomCheckBox?.isChecked = false
+                }
+                getString(R.string.safaricom) -> {
+                    airtelCheckBox?.isChecked = false
+                    safaricomCheckBox?.isChecked = true
+                    telcomCheckBox?.isChecked = false
+                }
+                getString(R.string.telkom) -> {
+                    airtelCheckBox?.isChecked = false
+                    safaricomCheckBox?.isChecked = false
+                    telcomCheckBox?.isChecked = true
+                }
+            }
+        }
+
+        binding.airtimeOperator.setOnClickListener {
+            bottomSheet.show()
+            update()
+        }
+
+        airtelLayout?.setOnClickListener {
+            airtelCheckBox?.isChecked = true
+            safaricomCheckBox?.isChecked = false
+            telcomCheckBox?.isChecked = false
+        }
+
+        safaricomLayout?.setOnClickListener {
+            airtelCheckBox?.isChecked = false
+            safaricomCheckBox?.isChecked = true
+            telcomCheckBox?.isChecked = false
+        }
+
+        telcomLayout?.setOnClickListener {
+            airtelCheckBox?.isChecked = false
+            safaricomCheckBox?.isChecked = false
+            telcomCheckBox?.isChecked = true
+        }
+
+        btnSelect?.setOnClickListener {
+            when {
+                airtelCheckBox?.isChecked == true -> {
+                    binding.airtimeOperator.text = getString(R.string.airtel)
+                    binding.airtimeIcon.setImageResource(R.drawable.airtel)
+                    bottomSheet.dismiss()
+                }
+                safaricomCheckBox?.isChecked == true -> {
+                    binding.airtimeOperator.text = getString(R.string.safaricom)
+                    binding.airtimeIcon.setImageResource(R.drawable.safaricom)
+                    bottomSheet.dismiss()
+                }
+                telcomCheckBox?.isChecked == true -> {
+                    binding.airtimeOperator.text = getString(R.string.telkom)
+                    binding.airtimeIcon.setImageResource(R.drawable.telcom)
+                    bottomSheet.dismiss()
+                }
+            }
+        }
     }
 
     override fun initListeners() {
